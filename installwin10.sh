504ce7978255c0e11780bc9ebda862a1e6b3ad7d
puarudz <phongpt.15th.0005@sv.daihoclongan.edu.vn>
2024-12-02 14:55:54 +0700

    Create installwin10.sh

---
+++ installwin10.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env sh
+if [ "$(id -u)" != "0" ]; then
+	echo "You must be root to execute the script. Exiting."
+	exit 1
+fi
+wget https://raw.githubusercontent.com/puarudz/install2012/main/ti.sh
+bash ti.sh https://archive.org/download/windows4vpsisorecoverrrq11/windows10.gz
