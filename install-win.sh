c11f5f901643345e635014e58db5626402421472
puarudz <phongpt.15th.0005@sv.daihoclongan.edu.vn>
2024-12-02 19:20:58 +0700

    Update install-win.sh

--- install-win.sh
+++ install-win.sh
@@ -6,12 +6,18 @@ if [ "$(id -u)" != "0" ]; then
 fi
 
 # Nhận đầu vào từ người dùng
-read -p "Nhập lựa chọn (10, 12, 16, 19, 22): " choice
+read -p "Nhập lựa chọn (7, 10, 11, 12, 16, 19, 22): " choice
 
 # Kiểm tra lựa chọn và xác định URL tương ứng
 case "$choice" in
+    7)
+        url="https://files.storage.vn.vsicloud.com/win7.gz"
+        ;;
     10)
         url="https://files.storage.vn.vsicloud.com/win10.gz"
+        ;;
+    11)
+        url="https://files.storage.vn.vsicloud.com/win11.gz"
         ;;
     12)
         url="https://files.storage.vn.vsicloud.com/win12.gz"
@@ -26,7 +32,7 @@ case "$choice" in
         url="https://files.storage.vn.vsicloud.com/win22.gz"
         ;;
     *)
-        echo "Lựa chọn không hợp lệ. Vui lòng nhập 10, 12, 16, 19 hoặc 22."
+        echo "Lựa chọn không hợp lệ. Vui lòng nhập 7, 10, 11, 12, 16, 19 hoặc 22."
         exit 1
         ;;
 esac
